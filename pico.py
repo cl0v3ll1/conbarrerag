03bf3a8c58ec6bca36ca23c563d448df1d7ec7f3
cl0v3ll1 <1tscl0v3ll1@gmail.com>
2025-04-15 13:20:46 -0400

    python primer clase

---
+++ pico.py
@@ -0,0 +1,52 @@
+# # promedio de 3 notas
+
+# print("Ingresaremos las 3 notas para sacar su promedio")
+
+# n1=int(input("Primer numero "))
+# n2=int(input("Segundo numero "))
+# n3=int(input("Tercer numero "))
+# promedio=(n1+n2+n3)/3
+# print("Su promedio es el siguiente",promedio)
+
+# if promedio>=40:
+#     print("El alumno aprobó")
+# else:
+#     print("El alumno reprobo")
+
+# # verificacion de mayoria de edad
+
+# edad=int(input("Ingrese su edad "))
+
+# if edad>=18:
+#     print("Usted es mayor de edad")
+# else:
+#     print("Usted es menor de edad")
+
+
+# # niño menor de 12
+# # a<dolescente entre 12 y 17
+# # mayor de edad más o igual a 18
+
+# # elif significa else y if en una misma palabra
+
+# edad=int(input("Cuantos años tienes? "))
+
+# if edad < 12:
+#     print("tienes",edad, "años por lo tanto es un niño")
+# elif edad >= 12 and edad <=17:
+#     print("Usted es un adolescente")
+# else:
+#     print("Eres mayor de edad")
+
+# # ingrese 3 numeros y muestre el mayor de ellos
+
+# n1 = int(input("Ingrese numero 1 "))
+# n2 = int(input("Ingrese numero 2 "))
+# n3 = int(input("Ingrese numero 3 "))
+
+# if n1>n2 and n1>n3:
+#     print("el numero", n1, "es el mayor")
+# elif n2>n3:
+#     print("el numero", n2, "es el mayor")
+# else:
+#     print("el numero", n3, "es el mayor")
